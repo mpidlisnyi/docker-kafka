8b1e4cc871bcc2d176501ff793d0445302d53b59
Maksym Pidlisnyi <maksim@nightbook.info>
2017-08-28 10:37:06 +0300

    remove -Xms

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export HEAP_SIZE=${HEAP_SIZE:=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes | awk '{printf("%.0fm\n",($1*0.8)/1024/1024)}'`}
-export KAFKA_HEAP_OPTS="-Xmx${HEAP_SIZE} -Xms${HEAP_SIZE}"
+export KAFKA_HEAP_OPTS="-Xmx${HEAP_SIZE}"
 
 if [ $SERVER_PROPERTIES_URL ];
 then
